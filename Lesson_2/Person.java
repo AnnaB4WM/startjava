1552c6db47d5cea51413edaad45bf653943537de
AnnaB <annab4wm@gmail.com>
2024-02-04 13:02:40 +0300

    Lesson_2 HW1 is done

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    String gender = "male";
+    String name = "Mark"
+    double height = 1.80;
+    double weight = 75.0;
+    int age = 40;
+
+    void move() {
+        System.out.println("Вы прошли 100 метров");
+    }
+
+    void sit() {
+        System.out.println("Вы сели на диван");
+    }
+
+    void run() {
+        System.out.println("Вы бегаете 3 раза в неделю");
+    }
+
+    void speak() {
+        System.out.println("Вы говорите по-английски");
+    }
+
+    boolean learnJava() {
+        System.out.println("Вы изучили 1 Урок");
+        return true;
+    }
+}
