2a117af65d778301f655eef2b41163b7e98c2437
AnnaB <annab4wm@gmail.com>
2024-02-14 17:20:16 +0300

    lesson_2_3 итоговая HW исправила замечания

--- src/com/startjava/lesson_2_3/rps/RpsGameFormatting.java
+++ src/com/startjava/lesson_2_3/rps/RpsGameFormatting.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 // Игра Камень-Ножницы-Бумага
 public class RpsGameFormatting {
 
+    private static final String ROCK = "R";
+    private static final String SCISSORS = "S";
+    private static final String PAPER = "P";
+
     public static void main(String[] args) throws InterruptedException {
         Random r = new Random();
         Scanner console = new Scanner(System.in);
@@ -24,10 +28,6 @@ public class RpsGameFormatting {
 
         defineWinner(name1, sign1, name2, sign2);
     }
-
-    private static final String ROCK = "R";
-    private static final String SCISSORS = "S";
-    private static final String PAPER = "P";
 
     private static String inputName(Scanner console) {
         System.out.println("Введите имя игрока: ");
